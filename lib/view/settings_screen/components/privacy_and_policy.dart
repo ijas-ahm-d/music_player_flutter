2050f6950698335d39b836f0914e571e5e0a4525
ijas-ahm-d <ijasahammed2000@gmail.com>
2023-09-11 13:59:53 +0530

    privacy policy changed

--- lib/view/settings_screen/components/privacy_and_policy.dart
+++ lib/view/settings_screen/components/privacy_and_policy.dart
@@ -21,7 +21,7 @@ class PrivacyPolicyPage extends StatelessWidget {
             Text("""
 Privacy Policy for AudioScape player
 
-Last updated: February 03, 2023
+Last updated: september 11, 2023
 
 This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.
 
